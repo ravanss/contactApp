d18dedf8a41f06e13da75cc76b623d6e7c812cfd
ravanss <psaulo7@gmail.com>
2024-03-17 00:44:15 -0300

    Adicionando acao entre as paginas de home e contatos com Router

--- contactApp/src/app/app.routes.ts
+++ contactApp/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path:"contatos",
+    component: ContatosComponent
   }
 ];
