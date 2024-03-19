84454629d9058d723dddb3e199b945ccef012589
ravanss <psaulo7@gmail.com>
2024-03-19 16:17:19 -0300

    adicionando o fontAwesome ao projeto com o auxilio do npm

--- contactApp/src/app/pages/home/home.component.ts
+++ contactApp/src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   constructor(private router: Router){}
+
+  arrowrightlong = faArrowRightLong;
 
   openContacts(){
     this.router.navigate(["contatos"]);
