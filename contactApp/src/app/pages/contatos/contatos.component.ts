4d89b77ef178614cf116b6760b7aa15c91cdaf6c
ravanss <psaulo7@gmail.com>
2024-03-19 16:38:13 -0300

    Finalizando o layout da pagina de contatos

--- contactApp/src/app/pages/contatos/contatos.component.ts
+++ contactApp/src/app/pages/contatos/contatos.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contatos',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.scss'
 })
 export class ContatosComponent {
-
+  faplus = faPlus;
+  faBars = faBars;
 }
