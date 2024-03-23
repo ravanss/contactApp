34c4f4ec070946841a1a1dd1b2abbe751557bae4
ravanss <psaulo7@gmail.com>
2024-03-23 14:28:34 -0300

    Formulario de pesquisa

--- contactApp/src/app/pages/contatos/contatos.component.ts
+++ contactApp/src/app/pages/contatos/contatos.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contatos',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.scss'
 })
 export class ContatosComponent {
   faplus = faPlus;
   faBars = faBars;
+
+  loginForm!: FormGroup;
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      nome: new FormControl('', [Validators.required]),
+      numero: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
+  }
 }
